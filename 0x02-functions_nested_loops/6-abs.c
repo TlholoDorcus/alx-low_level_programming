d787edf06ac8ec089feb70c6dfb8f68d00422000
Tlhologelo <tlholodorcus@gmail.com>
2023-02-21 15:48:26 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _abs - gets the absolute value of n
+ * @n: check parameter n
+ *
+ * Return: absolute value of integer.
+ */
+int _abs(int n)
+{
+int x = abs(n);
+
+return (x);
+}
