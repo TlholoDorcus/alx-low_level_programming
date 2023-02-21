6c6d7969890e4713b3a5e2e8da4da0a6b31dd48d
Tlhologelo <tlholodorcus@gmail.com>
2023-02-21 13:53:32 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _islower - checks whether alphabet is lower case or not
+ *
+ * Return: 1 for yes and 0 for no.
+ */
+int _islower(int c)
+{
+int x = 0;
+
+if (islower(c) > 0)
+x = 1;
+
+if (islower(c) == 0)
+x = 0;
+
+return(x);
+}
