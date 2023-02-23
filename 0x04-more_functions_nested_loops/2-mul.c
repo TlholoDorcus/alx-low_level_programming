debd51c2bfd0132617feacbef0421c8e18bbdd5a
Tlhologelo <tlholodorcus@gmail.com>
2023-02-23 05:53:38 -0800

    Collaboration

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * mul - multiplies 2 digits
+ * @a: int for multiplying
+ * @b: int for multiplying
+ *
+ * Return: On success 1.
+ */
+int mul(int a, int b)
+{
+	int x;
+
+	x = a * b;
+
+	return (x);
+}
