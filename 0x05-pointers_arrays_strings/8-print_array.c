3b645104c2a1847d3beb57cfca3f5b1d828cdc53
Tlhologelo <tlholodorcus@gmail.com>
2023-02-27 15:16:30 -0800

    Arrays are not pointers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_array - get length of string
+ * @a: string passed in
+ * @n: integer
+ *
+ * Return: 0 for success
+ */
+void print_array(int *a, int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+if (i == 5)
+break;
+
+if (i != 0)
+printf(", ");
+
+printf("%d ", *(a + i)); 
+}
+printf("\n");
+return;
+}
