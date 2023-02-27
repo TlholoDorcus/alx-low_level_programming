90a1db448829b1b3817d47b123e9a7b0e4895deb
Tlhologelo <tlholodorcus@gmail.com>
2023-02-27 15:45:05 -0800

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *_strcpy - copies stings ponted to by src
+ * @dest: string passed in
+ * @src: string passed in
+ *
+ * Return: dest - the pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+return (dest);
+}
