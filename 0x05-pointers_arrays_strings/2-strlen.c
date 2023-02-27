30750abfe936995dde99d9ed0f1df62742143b1a
Tlhologelo <tlholodorcus@gmail.com>
2023-02-27 07:17:51 -0800

    its very length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - get length of string
+ * @s: string passed in
+ *
+ * Return: c - length of string
+ */
+int _strlen(char *s)
+{
+int c = strlen(s);
+return(c);
+}
