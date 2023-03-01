a457ff4e6d118d36eb9b14a24fbf4654f0469719
Tlhologelo <tlholodorcus@gmail.com>
2023-03-01 10:49:05 -0800

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcmp- compares two strings
+ * @s1: string passed in
+ * @s2: string passed in
+ *
+ * Return: i for comparisons
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i = strcmp(s1, s2);
+return (i);
+}
