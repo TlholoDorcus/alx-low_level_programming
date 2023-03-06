fb7a154d83c4f3a80fc0d695d28f0c6e36b83642
Tlhologelo <tlholodorcus@gmail.com>
2023-03-06 07:48:09 -0800

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *_memset- fills memory with constant byte
+ * @s: parameter returned
+ * @b: byte that fills memory
+ * @n: number of times 
+ *
+ * Return: i for comparisons
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+memset(s, b, n);
+return (s);
+}
