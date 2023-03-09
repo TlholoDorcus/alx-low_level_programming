fae036b692593890c3755d6df91d5bfd4b276865
Tlhologelo <tlholodorcus@gmail.com>
2023-03-09 05:52:10 -0800

    locked away

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts_recursion - prints a string and a new line
+ * @c: parameter string
+ *
+ * return: nothing
+ */
+int _putchar(char c);
+void _puts_recursion(char *s)
+{
+puts(s);
+return;
+}
