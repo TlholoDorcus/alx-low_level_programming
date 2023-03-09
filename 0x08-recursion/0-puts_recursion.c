1ecd6018b4b28245d6282963e429fc7b093e061f
Tlhologelo <tlholodorcus@gmail.com>
2023-03-09 05:55:58 -0800

    locked away

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,7 +5,7 @@
  * _puts_recursion - prints a string and a new line
  * @c: parameter string
  *
- * return: nothing
+ * return: a value called c
  */
 int _putchar(char c);
 void _puts_recursion(char *s)
