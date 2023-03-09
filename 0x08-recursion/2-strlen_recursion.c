61d6338d49820cede62008878af0ee892572ab97
Tlhologelo <tlholodorcus@gmail.com>
2023-03-09 07:57:30 -0800

    Dreams feel real

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * int _strlen_recursion - prints length of string
+ * @s: parameter string
+ *
+ * return: length of string
+ *
+ */
+int _strlen_recursion(char *s)
+{
+int len = strlen(s);
+
+return(len);
+}
