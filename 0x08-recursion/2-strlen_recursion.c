a9bb089e70af34a9ead15e96fae9b63bbb86ca3f
Tlhologelo <tlholodorcus@gmail.com>
2023-03-09 11:42:09 -0800

    Dreams feel real

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,12 +5,12 @@
  * _strlen_recursion - prints length of string
  * @s: parameter string
  *
- * return: length of string
+ * return: len
  *
  */
 int _strlen_recursion(char *s)
 {
 int len = strlen(s);
 
-return(len);
+return (len);
 }
