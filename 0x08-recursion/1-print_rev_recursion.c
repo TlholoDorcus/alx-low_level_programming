ca101d0589f5c4ab53514ff60a66560b60656ac7
Tlhologelo <tlholodorcus@gmail.com>
2023-03-09 07:49:54 -0800

    important to dream

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: parameter string
+ *
+ * return: nothing
+ *
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+{
+return;
+}
+_print_rev_recursion(s + 1);
+putchar(*s);
+}
