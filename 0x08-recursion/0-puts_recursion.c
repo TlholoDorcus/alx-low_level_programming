8d933f610b65adaa9ea572b64228f92664e9d731
Tlhologelo <tlholodorcus@gmail.com>
2023-07-06 12:30:45 -0700

    A function that calls itself

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+#include "string.h"
+#include "main.h"
+
+/**
+ * _puts_recursion -  prints string follows by new line
+ * @s - string to be printed
+ *
+ * Return: nothing.
+ */
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+return;
+}
+_puts_recursion(s - 1);
+puts(s);
+}
