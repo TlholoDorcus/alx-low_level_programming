2ac8444b7336ded89ba7906deb54025e4360e52f
Tlhologelo <tlholodorcus@gmail.com>
2023-07-17 14:38:45 -0700

    Poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+First struct of structures
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+int _putchar(char c);
+
+/**
+ * struct dog my_dog - Defining a new struct
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: First time doing structs and new at this
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
